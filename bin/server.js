b1b8c0bba36b8bb33fa6cc06334e265852206773
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-11-06 15:03:10 +0200

    server

--- bin/server.js
+++ bin/server.js
@@ -1,23 +1,21 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+const app = require('../app');
+require('dotenv').config();
 
-const app = require('../app')
+const { DB_HOST, PORT = 4000 } = process.env;
 
-const PORT = process.env.PORT || 3000
-
-const { DB_HOST } = process.env
-
-mongoose.connect(DB_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Database connected successfully')
+    console.log('Database connected successfully');
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    })
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
   })
   .catch(error => {
-    console.log(error.message)
-    process.exit(1)
-  })
+    console.log(error.message);
+    process.exit(1);
+  });
