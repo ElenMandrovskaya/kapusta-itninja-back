48c11738887e2bacbaf86d979f81fd5cce0d10ce
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-11-06 15:02:56 +0200

    express, cors, routes in app

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
-const app = express()
+const authRouter = require('./routes/api/auth');
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const app = express();
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/auth', authRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({
+    status: 'error',
+    message: 'Not found',
+  });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  const { status = 500, message = err.message } = err;
+  res.status(status).json({ status: 'error', code: status, message });
+});
 
-module.exports = app
+module.exports = app;
