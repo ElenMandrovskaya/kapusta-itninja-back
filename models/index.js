edfa057fd6e2b9130a83091d84d93741744c2e8a
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-11-08 19:18:46 +0200

    added models for categories and transactions

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 const { User } = require('./user');
+const { Category } = require('./categories');
+const { Transaction } = require('./transactions');
 
 module.exports = {
   User,
+  Category,
+  Transaction,
 };
---
+++ models/transactions.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const transactionSchema = Schema(
+  {
+    date: {
+      day: String,
+      month: String,
+      year: String,
+    },
+    name: {
+      type: String,
+      required: [true, 'This field is required'],
+    },
+    value: {
+      type: Number,
+      required: [true, 'This field is required'],
+      default: 0.0,
+    },
+    expenses: {
+      type: Boolean,
+      default: false,
+    },
+    category: {
+      type: String,
+      required: [true, 'This field is required'],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+const transactionsSchemaJoi = Joi.object({
+  date: Joi.object({
+    day: Joi.string(),
+    month: Joi.string(),
+    year: Joi.string(),
+  }),
+  name: Joi.string().required(),
+  value: Joi.number().required(),
+  category: Joi.string(),
+  expenses: Joi.boolean(),
+});
+
+const Transaction = model('transaction', transactionSchema);
+
+module.exports = {
+  transactionsSchemaJoi,
+  Transaction,
+};
