10131081c6378afb7c9d1c56300a8756cf8bc573
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-11-10 22:10:23 +0200

    added controllers for income summary

--- controllers/reports/index.js
+++ controllers/reports/index.js
@@ -1,7 +1,10 @@
 const getExpenseReport = require('./getExpenseReport');
+const getIncomeReport = require('./getIncomeReport');
+
 // const getExpenseReportByMonth_Oks = require('./getExpenseReportByMonth_Oks');
 
 module.exports = {
   getExpenseReport,
+  getIncomeReport,
   // const getExpenseReportByMonth_Oks
 };
--- routes/api/reports.js
+++ routes/api/reports.js
@@ -4,6 +4,7 @@ const { reports: ctrl } = require('../../controllers');
 const { controllerWrapper, authenticate } = require('../../middlewares');
 
 router.get('/expense', authenticate, controllerWrapper(ctrl.getExpenseReport));
+router.get('/income', authenticate, controllerWrapper(ctrl.getIncomeReport));
 
 // router.get(
 //   '/expense/monthly',
