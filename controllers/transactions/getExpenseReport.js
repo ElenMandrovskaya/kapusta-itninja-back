9e826133dbf70931342206a15c68b1df4482b255
Oksana Zelenevskaya <73756266+OksanaZel@users.noreply.github.com>
2021-11-10 14:08:38 +0200

    added sort

--- controllers/transactions/getExpenseReport.js
+++ controllers/transactions/getExpenseReport.js
@@ -17,12 +17,19 @@ const getExpenseReport = async (req, res) => {
 
     reportArray.push(record);
   });
-  console.log(reportArray);
 
-  const finalReportArray = reportArray.reduce(
+  const reducer = reportArray.reduce(
     (acc, c) => ((acc[c.month] = (acc[c.month] || 0) + c.value), acc),
     {},
   );
+
+  const finalReportArray = Object.keys(reducer)
+    .map(item => ({
+      month: item,
+      value: reducer[item],
+    }))
+    .sort((a, b) => (a.month < b.month ? 1 : b.month < a.month ? -1 : 0));
+  console.log(finalReportArray);
 
   sendSuccessResponse(res, { finalReportArray }, 200);
 };
